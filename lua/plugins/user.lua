36c3710ad452193f951643f30b29f4f024eba10f
AlexFW <peherstorfer.alexander@gmail.com>
2026-07-18 22:22:06 +0200

    commented out dickvandyke opencode plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -370,57 +370,76 @@ return {
       -- configurations go here
     },
   },
--- Opencode (AI code actions)
-{
-  "nickjvandyke/opencode.nvim",
-  version = "*", -- Latest stable release
-  dependencies = {
-    {
-      -- `snacks.nvim` integration is recommended, but optional
-      ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
-      "folke/snacks.nvim",
-      optional = true,
-      opts = {
-        input = {}, -- Enhances `ask()`
-        picker = { -- Enhances `select()`
-          actions = {
-            opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
-          },
-          win = {
-            input = {
-              keys = {
-                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      -- Your configuration, if any; goto definition on the type or field for details
-    }
-
-    vim.o.autoread = true -- Required for `opts.events.reload`
-
-    -- Recommended/example keymaps
-    vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-    vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
-
-    vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
-    vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
-
-    vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
-    vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
-
-    -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
-    vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
-    vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
-  end,
-  },
+-- -- Opencode (AI code actions)
+-- {
+--   "nickjvandyke/opencode.nvim",
+--   version = "*", -- Latest stable release
+--   dependencies = {
+--     {
+--       -- `snacks.nvim` integration is recommended, but optional
+--       ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+--       "folke/snacks.nvim",
+--       optional = true,
+--       opts = {
+--         input = {}, -- Enhances `ask()`
+--         picker = { -- Enhances `select()`
+--           actions = {
+--             opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+--           },
+--           win = {
+--             input = {
+--               keys = {
+--                 ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+--               },
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+--   config = function()
+--     ---@type opencode.Opts
+--     vim.g.opencode_opts = {
+--       server = {
+--         start = function()
+--           local cwd = vim.fn.getcwd()
+--           local name = vim.fs.basename(cwd)
+--           require("opencode.terminal").open(
+--             ("docker run --rm -it -v %s:/repo -w /repo %s opencode --port"):format(cwd, name), {
+--               split = "right",
+--               width = math.floor(vim.o.columns * 0.35),
+--             })
+--         end,
+--         toggle = function()
+--           local cwd = vim.fn.getcwd()
+--           local name = vim.fs.basename(cwd)
+--           require("opencode.terminal").toggle(
+--             ("docker run --rm -it -v %s:/repo -w /repo %s opencode --port"):format(cwd, name), {
+--               split = "right",
+--               width = math.floor(vim.o.columns * 0.35),
+--             })
+--         end,
+--       },
+--     }
+--
+--     vim.o.autoread = true -- Required for `opts.events.reload`
+--
+--     -- Recommended/example keymaps
+--     vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+--     vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+--     vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+--
+--     vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+--     vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+--
+--     vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+--     vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+--
+--     -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+--     vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+--     vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+--   end,
+--   },
 
 
   -- TODO: move to mappings?
